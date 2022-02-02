32890011d06c8ec0cd5daf8c832064532a98054c
fernandofrantz <fernandofrantz12@gmail.com>
2022-02-02 10:13:56 -0300

    comandos revisados

--- parte-01.sql
+++ parte-01.sql
@@ -1,25 +1,25 @@
 CREATE TABLE IF NOT EXISTS redes_sociais(
-    id PRIMARY KEY SERIAL
+    id BIGSERIAL PRIMARY key,
     nome VARCHAR(150) NOT NULL UNIQUE
 )
 
 CREATE TABLE IF NOT EXISTS enderecos(
-    id PRIMARY KEY SERIAL
-    rua VARCHAR NOT NULL
-    pais VARCHAR(100) NOT NULL
+    id BIGSERIAL PRIMARY KEY,
+    rua VARCHAR NOT NULL,
+    pais VARCHAR(100) NOT NULL,
     cidade VARCHAR(100) NOT NULL
 )
 
 CREATE TABLE IF NOT EXISTS usuarios(
-    id PRIMARY KEY SERIAL
-    nome VARCHAR(100)
-    email VARCHAR NOT NULL UNIQUE
-    senha VARCHAR NOT NULL
-    endereco_id REFERENCES enderecos(id)
+    id BIGSERIAL PRIMARY KEY,
+    nome VARCHAR(100),
+    email VARCHAR NOT NULL UNIQUE,
+    senha VARCHAR NOT NULL,
+    endereco_id INT NOT NULL REFERENCES enderecos(id)
 )
 
 CREATE TABLE IF NOT EXISTS usuario_rede_sociais(
-    id PRIMARY KEY SERIAL
-    usuario_id REFERENCES usuarios(id)
-    redes_social_id REFERENCES redes_sociais(id)
+    id BIGSERIAL PRIMARY KEY,
+    usuario_id INT NOT NULL REFERENCES usuarios(id),
+    redes_social_id INT NOT NULL REFERENCES redes_sociais(id)
 )
--- parte-02.sql
+++ parte-02.sql
@@ -8,10 +8,10 @@ VALUES
 INSERT INTO 
     usuarios(nome, email, senha, endereco_id)
 VALUES
-    ('Cauan', 'cauan@exemple.com', '1234', SELECT id FROM enderecos WHERE enderecos.cidade === 'São Paulo'),
-    ('Chrystian', 'chrystian@exemple.com', '4321', SELECT id FROM enderecos WHERE enderecos.cidade === 'Curitiba'),
-    ('Matheus', 'matheus@exemple.com', '3214', SELECT id FROM enderecos WHERE enderecos.cidade === 'Londrina')
-
+    ('Cauan', 'cauan@exemple.com', '1234', (SELECT id FROM enderecos WHERE enderecos.cidade = 'São Paulo')),
+    ('Chrystian', 'chrystian@exemple.com', '4321', (SELECT id FROM enderecos WHERE enderecos.cidade = 'Curitiba')),
+    ('Matheus', 'matheus@exemple.com', '3214', (SELECT id FROM enderecos WHERE enderecos.cidade = 'Londrina'))
+ 
 INSERT INTO 
     redes_sociais(nome)
 VALUES
@@ -21,16 +21,18 @@ VALUES
     ('Facebook'),
     ('TikTok')
 
+
 INSERT INTO 
     usuario_rede_sociais(usuario_id, redes_social_id)
 VALUES
-    (SELECT id FROM usuarios WHERE usuarios.name === 'Cauan', SELECT id FROM redes_sociais WHERE redes_sociais.name === 'Youtube'),
-    (SELECT id FROM usuarios WHERE usuarios.name === 'Chrystian', SELECT id FROM redes_sociais WHERE redes_sociais.name === 'Youtube'),
-    (SELECT id FROM usuarios WHERE usuarios.name === 'Matheus', SELECT id FROM redes_sociais WHERE redes_sociais.name === 'Youtube'),
-    (SELECT id FROM usuarios WHERE usuarios.name === 'Chrystian', SELECT id FROM redes_sociais WHERE redes_sociais.name === 'Twitter'),
-    (SELECT id FROM usuarios WHERE usuarios.name === 'Cauan', SELECT id FROM redes_sociais WHERE redes_sociais.name === 'Twitter'),
-    (SELECT id FROM usuarios WHERE usuarios.name === 'Matheus', SELECT id FROM redes_sociais WHERE redes_sociais.name === 'Instagram'),
-    (SELECT id FROM usuarios WHERE usuarios.name === 'Matheus', SELECT id FROM redes_sociais WHERE redes_sociais.name === 'Facebook'),
-    (SELECT id FROM usuarios WHERE usuarios.name === 'Chrystian', SELECT id FROM redes_sociais WHERE redes_sociais.name === 'Instagram'),
-    (SELECT id FROM usuarios WHERE usuarios.name === 'Cauan', SELECT id FROM redes_sociais WHERE redes_sociais.name === 'TikTok'),
+    ((SELECT id FROM usuarios WHERE usuarios.nome = 'Cauan'), (SELECT id FROM redes_sociais WHERE redes_sociais.nome = 'Youtube')),
+    ((SELECT id FROM usuarios WHERE usuarios.nome = 'Chrystian'), (SELECT id FROM redes_sociais WHERE redes_sociais.nome = 'Youtube')),
+    ((SELECT id FROM usuarios WHERE usuarios.nome = 'Matheus'), (SELECT id FROM redes_sociais WHERE redes_sociais.nome = 'Youtube')),
+    ((SELECT id FROM usuarios WHERE usuarios.nome = 'Chrystian'), (SELECT id FROM redes_sociais WHERE redes_sociais.nome = 'Twitter')),
+    ((SELECT id FROM usuarios WHERE usuarios.nome = 'Cauan'), (SELECT id FROM redes_sociais WHERE redes_sociais.nome = 'Twitter')),
+    ((SELECT id FROM usuarios WHERE usuarios.nome = 'Matheus'), (SELECT id FROM redes_sociais WHERE redes_sociais.nome = 'Instagram')),
+    ((SELECT id FROM usuarios WHERE usuarios.nome = 'Matheus'), (SELECT id FROM redes_sociais WHERE redes_sociais.nome = 'Facebook')),
+    ((SELECT id FROM usuarios WHERE usuarios.nome = 'Chrystian'), (SELECT id FROM redes_sociais WHERE redes_sociais.nome = 'Instagram')),
+    ((SELECT id FROM usuarios WHERE usuarios.nome = 'Cauan'), (SELECT id FROM redes_sociais WHERE redes_sociais.nome = 'TikTok'))
+
 
